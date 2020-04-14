0b9262b6175a29a0e6d2f6c8de0401904a72b880
Sean Xiong <seanxionghe@gmail.com>
2020-04-14 20:00:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,4 @@
 # code your #position_taken? method here!
-def position_taken?(array, index)
-  if array[index] == "X" || "O"
-    return true
-  else
-    return false
-  end
+def position_taken?(board, index)
+
 end
